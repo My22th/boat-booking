a86cc728b92104a80f69912b69ed22b1ae420d7e
long <longlop68@gmail.com>
2023-09-17 00:18:21 +0700

    e

--- BookingAppWebApi/BookingAppWebApi/Model/MongoDBFD/BoatBookingMgDatabase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Booking_App_WebApi.Model.MongoDBFD
-{
-    public interface IBoatBookingMgDatabase
-    {
-    }
-    public class BoatBookingMgDatabase: IBoatBookingMgDatabase
-    {
-    }
-}
